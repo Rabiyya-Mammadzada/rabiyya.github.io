459e9a305947304381df255d878936b685b8d07c
Rabiyya Mammadzada <rabiamammedzade@gmail.com>
2022-04-07 09:25:33 +0400

    commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,96 +1,101 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faPencil,
-  faTrash,
-  faCheck,
-  faFloppyDisk,
+    faPencil,
+    faTrash,
+    faCheck,
+    faFloppyDisk,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState, useRef, useEffect, useContext } from "react";
 import { TodoContext } from "./Context";
 
-const Todo = ({ todos, setTodos, todo, title,completed }) => {
-  const [edit, setEdit] = useState(false);
-  const input = useRef();
+const Todo = ({ todos, setTodos, todo, title, completed }) => {
+    const [edit, setEdit] = useState(false);
+    const input = useRef();
 
-  useEffect(() => {
-    if (edit === true) {
-      input.current.focus();
-    }
-  }, [edit]);
-
-  const completeHandler = () => {
-    setTodos(
-        todos.map((item) => {
-        if (item.id === todo.id) {
-          return {
-            ...item,
-            completed: !item.completed,
-          };
+    useEffect(() => {
+        if (edit === true) {
+            input.current.focus();
         }
-        return item;
-      })
-    );
-  };
+    }, [edit]);
 
-  const removeHandler = () => {
-    setTodos(todos.filter((el) => el.id !== todo.id));
-  };
-
-  const editInputHandler = () => {
-    setEdit(true);
-  };
-
-  const saveInputHandler = () => {
-    setEdit(false);
-  };
-
-  const editedTodo = (e) => {
-    setTodos(
-        todos.map((item) => {
-        if (item.id === todo.id) {
-          return {
-            ...item,
-            title: e.target.value,
-          };
+    const completeHandler = () => {
+        if (edit) {
+            alert("Please save it")
+        } else {
+            setTodos(
+                todos.map((item) => {
+                    if (item.id === todo.id) {
+                        return {
+                            ...item,
+                            completed: !item.completed,
+                        };
+                    }
+                    return item;
+                })
+            );
         }
-        return item;
-      })
+
+    };
+
+    const removeHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+
+    const editInputHandler = () => {
+        setEdit(true);
+    };
+
+    const saveInputHandler = () => {
+        setEdit(false);
+    };
+
+    const editedTodo = (e) => {
+        setTodos(
+            todos.map((item) => {
+                if (item.id === todo.id) {
+                    return {
+                        ...item,
+                        title: e.target.value,
+                    };
+                }
+                return item;
+            })
+        );
+    };
+
+    return (
+        <div className={`todo ${todo.completed ? "completed" : " "}`}>
+            <li className="todo-item">
+                {edit ? (
+                    <input
+                        type="text"
+                        onChange={editedTodo}
+                        className="edit-input"
+                        ref={input}
+                        value={title}
+                    />
+                ) : (
+                    title
+                )}
+            </li>
+            {edit ? (
+                <button className="save-btn" onClick={saveInputHandler}>
+                    <FontAwesomeIcon icon={faFloppyDisk} />
+                </button>
+            ) : (
+                <button className="edit-btn" onClick={editInputHandler}>
+                    <FontAwesomeIcon icon={faPencil} />
+                </button>
+            )}
+
+            <button className="complete-btn" onClick={completeHandler}>
+                <FontAwesomeIcon icon={faCheck} />
+            </button>
+            <button className="trash-btn" onClick={removeHandler}>
+                <FontAwesomeIcon icon={faTrash} />
+            </button>
+        </div>
     );
-  };
-
-  return (
-    <div className={`todo ${todo.completed ? "completed" : " "}`}>
-      <li className="todo-item">
-        {edit ? (
-          <input
-            type="text"
-            onChange={editedTodo}
-            className="edit-input"
-            ref={input}
-            value={title}
-          />
-        ) : (
-          title
-        )}
-      </li>
-      {edit ? (
-        <button className="save-btn" onClick={saveInputHandler}>
-          <FontAwesomeIcon icon={faFloppyDisk} />
-        </button>
-      ) : (
-        <button className="edit-btn" onClick={editInputHandler}>
-          <FontAwesomeIcon icon={faPencil} />
-        </button>
-      )}
-
-      <button className="complete-btn" onClick={completeHandler}>
-        <FontAwesomeIcon icon={faCheck} />
-      </button>
-      <button className="trash-btn" onClick={removeHandler}>
-        <FontAwesomeIcon icon={faTrash} />
-      </button>
-    </div>
-  );
 };
 
 export default Todo;
